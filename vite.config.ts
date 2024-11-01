bf37d2cea914e37750389707b8d3983788614bdb
aisuperchargedlearning <157744321+aisuperchargedlearning@users.noreply.github.com>
2024-10-31 17:36:06 -0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Required for Amplify deployment
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 });
